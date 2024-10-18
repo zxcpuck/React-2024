c7087409a39d37c8a900e90b680f9d3265ba06fa
zxcpuk <grayfruittop@gmail.com>
2024-10-18 15:39:15 +0300

    hw3

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import { routes } from './routes/routes';
 
 const root = ReactDOM.createRoot(
@@ -12,6 +12,3 @@ root.render(
     <RouterProvider router={routes}/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
